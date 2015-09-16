d0a63abddd2991c8a3397c3ce56749eff4e82837
Chris <cdhuizenga@gmail.com>
2015-09-16 16:53:01 -0400

    Prototype
    Initial Prototype build

---
+++ src/TwinStickShooter/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float movspd = 0;
+	bool moving = false;
+	float horspd = 0;
+	float vertspd = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float hdir = Input.GetAxisRaw ("Horizontal");
+		float vdir = Input.GetAxisRaw ("Vertical");
+		Vector3 hpos = transform.position;
+		Vector3 vpos = transform.position;
+		if (hdir != 0 && vdir != 0) {
+			moving = true
+		}
+		else 
+		{
+			moving = false;
+		}
+		if (moving == true)
+		{
+			if (hdir > 0)
+			{
+				transform.position = pos2;
+			}
+		}
+		
+	}
+}
